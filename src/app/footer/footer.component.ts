7255bc9692f5e3d5ed19798e116c887563f46e5b
Felix Brucker <felix@felixbrucker.com>
2022-01-21 19:29:58 +0100

    Fix footer year

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {faDiscord, faGithub, faPaypal} from "@fortawesome/free-brands-svg-icons";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   public faDiscord = faDiscord;
   public faGithub = faGithub;
   public faPaypal = faPaypal;
 
-  ngOnInit() {
-  }
-
+  public currentYear: string = moment().format('YYYY');
 }
