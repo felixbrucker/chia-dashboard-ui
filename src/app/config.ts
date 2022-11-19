fe9babf4cb8b25b3d616ffff400c42684f5ff732
Felix Brucker <felix@felixbrucker.com>
2022-11-19 13:13:35 +0100

    Use one of three api urls

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,7 +3,15 @@ export const githubClientId = '16645067d609075c61e4';
 // export const githubClientId = null;
 export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
 // export const googleClientId = null;
-export const apiBaseUrl = 'https://chia-dashboard-api.foxypool.io/api';
+const apiBaseUrls: string[] = [
+  'https://chia-dashboard-api.foxypool.io/api',
+  'https://chia-dashboard-api-2.foxypool.io/api',
+  'https://chia-dashboard-api-3.foxypool.io/api',
+]
+function randomArrayPosition<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]
+}
+export const apiBaseUrl = randomArrayPosition(apiBaseUrls)
 export const enablePeriodicUpdates = true;
 export const requestDiscordGuildPermission = true;
 
