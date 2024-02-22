ccee3dfe84ff5713f35a04813af6c46611b6cdd7
Felix Brucker <felix@felixbrucker.com>
2024-02-22 10:52:03 +0700

    Use HA endpoint

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,15 +3,7 @@ export const githubClientId = '16645067d609075c61e4';
 // export const githubClientId = null;
 export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
 // export const googleClientId = null;
-const apiBaseUrls: string[] = [
-  'https://chia-dashboard-api.foxypool.io/api',
-  'https://chia-dashboard-api-2.foxypool.io/api',
-  'https://chia-dashboard-api-3.foxypool.io/api',
-]
-function randomArrayPosition<T>(array: T[]): T {
-  return array[Math.floor(Math.random() * array.length)]
-}
-export const apiBaseUrl = randomArrayPosition(apiBaseUrls)
+export const apiBaseUrl = 'https://chia-dashboard-api.foxypool.io/api'
 export const requestDiscordGuildPermission = true;
 
 // DEV
