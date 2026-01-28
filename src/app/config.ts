1fb67ab38eb8b4ff60c7c5d9408541722ea5eccb
Felix Brucker <felix@felixbrucker.com>
2026-01-28 16:05:24 +0800

    Update google client id

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 export const discordClientId = '834108043687231570';
 export const githubClientId = '16645067d609075c61e4';
 // export const githubClientId = null;
-export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
+export const googleClientId = '520269672954-2jpls47hhgl5kqa0mjp36kt39fc5ubai.apps.googleusercontent.com';
 // export const googleClientId = null;
 export const apiBaseUrl = 'https://chia-dashboard-api.foxypool.io/api'
 export const requestDiscordGuildPermission = true;
